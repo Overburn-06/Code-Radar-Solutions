f2cf826212183ee3082f81c034a5d01d5089f4fa
Overburn-06 <189280292+Overburn-06@users.noreply.github.com>
2025-02-01 21:10:07 +0530

    Solution to Logical OR Operation

--- Logical OR Operation@@67882a731b4392d8fc1577dd/code.c
+++ Logical OR Operation@@67882a731b4392d8fc1577dd/code.c
@@ -7,15 +7,15 @@ char* welcome() {
 int main() {
     int a,b;
     scanf("%d%d",&a,&b);
-    if(a||b>0){
-        printf("True");
-    }
-    else if((a==b)&&(a&&b>0)){
+    // if(a||b>0){
+    //     printf("True");
+    // }
+    if((a==b)&&(a&&b>0)){
         printf("True");
 
     }
-    else{
-        printf("False");
-    }
+    // else{
+    //     printf("False");
+    // }
     return 0;
 }
