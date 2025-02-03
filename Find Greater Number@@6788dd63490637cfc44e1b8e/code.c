30ca3bd73b7478c4163fe0fc8eb7e27aae0b30b9
Overburn-06 <189280292+Overburn-06@users.noreply.github.com>
2025-02-03 13:05:15 +0530

    Solution to Find Greater Number

---
+++ Find Greater Number@@6788dd63490637cfc44e1b8e/code.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+char* welcome() {
+    return "Welcome to Code Radar!";
+}
+
+int main() {
+    int a,b;
+    scanf("%d%d",&a,&b);
+    if(a>b){
+        printf("%d",a);
+    }
+    else if(a==b){
+        printf("%d",a);
+    }
+    else{
+        printf("%d",b);
+    }
+   
+    return 0;
+}
