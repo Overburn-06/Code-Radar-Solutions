e1dc55153c4b6425de1ef862c1aace5971eeb016
Overburn-06 <189280292+Overburn-06@users.noreply.github.com>
2025-02-05 17:33:41 +0530

    Solution to Check Character Type

--- Check Character Type@@6788dd63490637cfc44e1b95/code.c
+++ Check Character Type@@6788dd63490637cfc44e1b95/code.c
@@ -13,9 +13,9 @@ int main() {
     else if (ch!='a' || ch!='e' || ch!='i'|| ch!='o'|| ch!='u' || ch!='A' || ch!='E' || ch!='I'|| ch!='O' ch!= 'U'){
         printf("Consonant");
     }
-    else if(ch>=1){
-        printf("Digit");
-    }
+    // else if(ch>=1){
+    //     printf("Digit");
+    // }
     // else{
     //     printf("Special Character");
     // }
