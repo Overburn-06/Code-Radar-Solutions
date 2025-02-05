a2ba8622b15eaf3395922dc41004fd46ecc5445e
Overburn-06 <189280292+Overburn-06@users.noreply.github.com>
2025-02-05 17:29:57 +0530

    Solution to Check Character Type

--- Check Character Type@@6788dd63490637cfc44e1b95/code.c
+++ Check Character Type@@6788dd63490637cfc44e1b95/code.c
@@ -7,7 +7,7 @@ char* welcome() {
 int main() {
     char ch;
     scanf("%c",&ch);
-    if (ch=='a' || ch=='e' || ch=='i'|| ch=='o'|| ch=='u'){
+    if (ch=='a' || ch=='e' || ch=='i'|| ch=='o'|| ch=='u'|| ch=='A' || ch=='E' || ch=='I' || ch=='O' || ch=='U'){
         printf("Vowel");
     }
     else if (ch!='a' || ch!='e' || ch!='i'|| ch!='o'|| ch!='u'){
