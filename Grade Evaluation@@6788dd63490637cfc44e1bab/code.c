c28a9a21d0cb87c4782abe56c373e780442aeda3
Overburn-06 <189280292+Overburn-06@users.noreply.github.com>
2025-02-06 18:41:50 +0530

    Solution to Grade Evaluation

---
+++ Grade Evaluation@@6788dd63490637cfc44e1bab/code.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+char* welcome() {
+    return "Welcome to Code Radar!";
+}
+
+int main() {
+    int a;
+    scanf("%c",&a);
+    if(a=='A'){
+        printf("Excellent");
+    }
+    else if(a=='B'){
+        printf("Good");
+    }
+    else if(a=='C'){
+        printf("Average");
+    }
+    else if(a=='D'){
+        printf("Below");
+    }
+    else if(a=='F'){
+        printf("Fail");
+    }
+    
+    return 0;
+}
