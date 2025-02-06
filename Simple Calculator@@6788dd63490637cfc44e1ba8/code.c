a912a9f50c1e05f4e5820e9c6d2301e7b6a7df34
Overburn-06 <189280292+Overburn-06@users.noreply.github.com>
2025-02-06 18:00:21 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -7,16 +7,16 @@ char* welcome() {
 int main() {
     int a,b,c;
     scanf("%d%d%d",&a,&b,&c);
-    if(c== +){
+    if(c== '+'){
         printf("%d",a+b);
     }
-    else if(c== -){
+    else if(c== '-'){
         printf("%d",a-b);
     }
-    else if(c== /){
+    else if(c== '/'){
         printf("%d",a/b);
     }
-    else if(c== *){
+    else if(c== '*'){
         printf("%d",a*b);
     }
     else{
