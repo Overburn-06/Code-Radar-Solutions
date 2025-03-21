91dd06784837f7d5a0b543d80bf57206c618718d
Agam Bhardwaj <189280292+Overburn-06@users.noreply.github.com>
2025-03-21 16:42:15 +0530

    Solution to Fibonacci Series

--- Fibonacci Series@@67ca7acd1fe08d7207c253c6/code.c
+++ Fibonacci Series@@67ca7acd1fe08d7207c253c6/code.c
@@ -1,6 +1,6 @@
 // Your code here...
 int fibonacciSeries(int n){
-    if(n==0||n==1) return 1 ;
+    if(n==1||n==2) return 1 ;
     int fibo= fibonacciSeries(n-1) + fibonacciSeries(n-2) ;
     printf("%d ",fibo);
     
