f5bdf70bc8f74c4580fcb4fa2a4c7bca2d50e5a1
Agam Bhardwaj <189280292+Overburn-06@users.noreply.github.com>
2025-03-21 16:53:27 +0530

    Solution to Fibonacci Series

--- Fibonacci Series@@67ca7acd1fe08d7207c253c6/code.c
+++ Fibonacci Series@@67ca7acd1fe08d7207c253c6/code.c
@@ -4,10 +4,14 @@ void fibonacciSeries(int n){
     int b=1;
     int sum;
     for(int i=1; i<=n-2;i++){
+        if(i==1) printf("0");
+        else if(i==2) printf("1");
+        else{
         sum=a+b;
         a=b;
         b=sum;
         printf("%d ",sum);
+        }
     }
     return;
 }
